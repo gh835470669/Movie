65e426833c0186bf94233dc42149ac7c2970a7cc
MikeWang66 <1173360606@qq.com>
2017-06-21 18:28:47 +0800

    Add files via upload
    影院功能部分

---
+++ src/main/java/com/example/activity/Activity_Search.java
@@ -0,0 +1,16 @@
+package com.example.activity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+/**
+ * Created by Wang on 2017/6/21.
+ */
+
+public class Activity_Search extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cinema);
+    }
+}
